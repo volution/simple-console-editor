303b055353e4b96439d3e5f1c2ba8fb17315fe8b
Ciprian Dorin, Craciun <ciprian@volution.ro>
2009-07-26 18:46:36 +0300

    Refactored scroll unicode string coercing. (Also I think there is a lurking bug about this.)

--- sce.bash
+++ sce.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-export PYTHONPATH="$( dirname "$( realpath "${0}" )" )" || exit 1
-exec python2.5 -m sce "${@}" || exit 1
+set -e -u -o pipefail || exit 1
+
+sources="$( dirname "$( realpath "${0}" )" )"
+
+test -f "${sources}/sce.py"
+
+if test "${#}" -eq 0
+then
+	exec python2.5 -E -O -O -u "${sources}/sce.py"
+else
+	exec python2.5 -E -O -O -u "${sources}/sce.py" "${@}"
+fi
 exit 1
--- sce_scroll.py
+++ sce_scroll.py
@@ -44,7 +44,7 @@ class Scroll :
 	
 	def update (self, _index, _string) :
 		self._touched = True
-		_string = unicode (_string)
+		_string = self.coerce (_string)
 		if self._lines == None :
 			self._lines = [_string]
 		else :
@@ -52,7 +52,7 @@ class Scroll :
 	
 	def append (self, _string) :
 		self._touched = True
-		_string = unicode (_string)
+		_string = self.coerce (_string)
 		if self._lines == None :
 			self._lines = [_string]
 		else :
@@ -60,7 +60,7 @@ class Scroll :
 	
 	def include_before (self, _index, _string) :
 		self._touched = True
-		_string = unicode (_string)
+		_string = self.coerce (_string)
 		if self._lines == None :
 			self._lines = [_string]
 		else :
@@ -68,7 +68,7 @@ class Scroll :
 	
 	def include_after (self, _index, _string) :
 		self._touched = True
-		_string = unicode (_string)
+		_string = self.coerce (_string)
 		if self._lines == None :
 			self._lines = [_string]
 		else :
@@ -91,7 +91,8 @@ class Scroll :
 		if self._lines == None :
 			self._lines = []
 		for _string in _strings :
-			self._lines.insert (_index, unicode (_string))
+			_string = self.coerce (_string)
+			self._lines.insert (_index, _string)
 			_index += 1
 	
 	def include_all_after (self, _index, _strings) :
@@ -99,15 +100,17 @@ class Scroll :
 		if self._lines == None :
 			self._lines = []
 		for _string in _strings :
-			self._lines.insert (_index + 1, unicode (_string))
+			_string = self.coerce (_string)
+			self._lines.insert (_index + 1, _string)
 			_index += 1
 	
-	def append_all (self, _string) :
+	def append_all (self, _strings) :
 		self._touched = True
 		if self._lines == None :
 			self._lines = []
 		for _string in _strings :
-			self._lines.append (unicode (_string))
+			_string = self.coerce (_string)
+			self._lines.append (_string)
 	
 	def split (self, _index, _column) :
 		self._touched = True
@@ -132,17 +135,18 @@ class Scroll :
 	
 	def insert (self, _index, _column, _string) :
 		self._touched = True
+		_string = self.coerce (_string)
 		if self._lines == None :
 			self._lines = [u'']
 		_line = self._lines[_index]
 		if (_column == 0) :
-			_line = unicode (_string) + _line
+			_line = _string + _line
 		elif (_column < len (_line)) :
-			_line = _line[: _column] + unicode (_string) + _line[_column :]
+			_line = _line[: _column] + _string + _line[_column :]
 		elif (_column == len (_line)) :
-			_line = _line + unicode (_string)
+			_line = _line + _string
 		else :
-			_line = _line + (' ' * (_column - len (_line))) + unicode (_string)
+			_line = _line + (' ' * (_column - len (_line))) + _string
 		self._lines[_index] = _line
 	
 	def delete (self, _index, _column, _length) :
@@ -159,4 +163,13 @@ class Scroll :
 		else :
 			_line = _line[: _column] + _line[_column + _length :]
 		self._lines[_index] = _line
+	
+	def coerce (self, _string) :
+		if isinstance (_string, unicode) :
+			pass
+		elif isinstance (_string, str) :
+			_string = unicode (_string, 'utf-8', 'replace')
+		else :
+			_string = unicode (str (_string))
+		return _string
 #
