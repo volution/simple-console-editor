6635ebe8e330679278f01366d3a583198619a6fe
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-10-11 16:04:10 +0300

    Minor update

--- sce.bash
+++ sce.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -e -u -o pipefail || exit 1
+set -e -E -u -o pipefail -o noclobber -o noglob +o braceexpand || exit 1
+trap 'printf "[ee] failed: %s\n" "${BASH_COMMAND}" >&2' ERR || exit 1
 
-sources="$( dirname "$( readlink -e "${0}" )" )"
+sources="$( dirname -- "$( readlink -e -- "${0}" )" )"
 
 test -f "${sources}/sce.py"
 
