25ea0a94f086ca84aa75be5c7f90f7b3f5e84426
Ciprian Dorin Craciun <ciprian@volution.ro>
2022-01-23 18:31:55 +0200

    [main]  Move main scripts to top of sources folder, so we don't have to fiddle with import path.

--- sources/editor_main.py
+++ sources/editor_main.py
@@ -1,15 +1,8 @@
 
-
 if __name__ == "__main__" :
-	
-	import sys, os
-	if "SCE_SOURCES" in os.environ :
-		sys.path.append (os.environ["SCE_SOURCES"])
-	
 	import editor, commands
 	commands.main (editor.main)
 	raise Exception ("566b6ea5")
-	
 else :
 	raise Exception ("4f67e656")
 
